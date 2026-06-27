7c997fd439d0e946ddf7ad9ac22c067fba2e7ed0
petrasvestartas <petrasvestartas@gmail.com>
2026-06-27 21:36:52 +0200

    Bump version to 0.1.17

--- src/compas_occt/__init__.py
+++ src/compas_occt/__init__.py
@@ -4,7 +4,7 @@ __author__ = ["Petras Vestartas"]
 __copyright__ = "Petras Vestartas"
 __license__ = "MIT License"
 __email__ = "petrasvestartas@gmail.com"
-__version__ = "0.1.16"
+__version__ = "0.1.17"
 
 
 HERE = os.path.dirname(__file__)
